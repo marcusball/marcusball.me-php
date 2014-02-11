6d358962541280232ce1241fa8944950d69a97e9
Marcus Ball <marcus.ball@live.com>
2014-02-11 15:57:53 -0500

    Updated header and footer include files; modified index.php to use include header/footer

--- www/page-include/footer.php
+++ www/page-include/footer.php
@@ -1,4 +1,30 @@
-			</div>
 		</div>
+		<footer class="row">
+			<div class="large-12 columns">
+				<hr />
+				<div class="row">
+					<div class="large-6 columns">
+						<p>© Marcus Ball</p>
+					</div>
+					<!--<div class="large-6 columns">
+						<ul class="inline-list right">
+							<li><a href="#">Link 1</a></li>
+							<li><a href="#">Link 2</a></li>
+							<li><a href="#">Link 3</a></li>
+							<li><a href="#">Link 4</a></li>
+							<li><a href="#">Link 5</a></li>
+							<li><a href="#">Link 6</a></li>
+							<li><a href="#">Link 7</a></li>
+						</ul>
+					</div>-->
+				</div>
+			</div> 
+		</footer>
+
+		<script src="/res/scripts/jquery.min.js"></script>
+		<script src="/res/scripts/foundation/foundation.min.js"></script>
+		<script>
+		$(document).foundation();
+		</script>
 	</body>
 </html>
