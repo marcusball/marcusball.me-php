b2e6a1238cf0a5c646b0acb24449710a6fc18e50
Marcus Ball <marcus.ball@live.com>
2014-02-11 15:57:08 -0500

    changed site name and details in serverconfig

--- www/serverconfig.php
+++ www/serverconfig.php
@@ -1,11 +1,11 @@
 <?php
 
-define('SITE_LABEL','sitename');
-define('SITE_NAME','Site Name');
-define('SITE_DOMAIN','www.dogesite.net');
-define('DB_PDO_NAME','postgres'); // The PDO name for your database server
-define('DB_NAME', 'sitedb');
-define('DB_USER', 'sitesqluser');
+define('SITE_LABEL','marcusball');
+define('SITE_NAME','Marcus Ball');
+define('SITE_DOMAIN','www.marcusball.me');
+define('DB_PDO_NAME','pgsql'); // The PDO name for your database server
+define('DB_NAME', '');
+define('DB_USER', '');
 define('DB_PASSWORD', 'doge1');
 define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
